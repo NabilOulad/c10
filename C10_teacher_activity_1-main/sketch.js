58fdd985b0d4d5b29acf0556c6f59e77f09fd823
NabilOulad <97562807+NabilOulad@users.noreply.github.com>
2022-01-13 16:50:17 -0600

    Add files via upload

---
+++ C10_teacher_activity_1-main/sketch.js
@@ -0,0 +1,18 @@
+var arr1 = [88,899,23,45,78,65,18,496]
+console.log(arr1);
+
+var arr2 = ["Nabil", 24 ,55, true,null]
+console.log(arr2)
+console.log(arr2[0]);
+
+function preload(){
+  //load the images, sounds, animations
+}
+
+function setup() {
+  createCanvas(600, 200);
+}
+
+function draw() {
+  background(220);
+}
